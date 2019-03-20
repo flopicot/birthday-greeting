417aae1b4447a3bbf9ec25186d8e2422d11dfaa1
Florent PICOT <fpicot@visiativ.com>
2019-03-20 10:59:21 +0100

    Fix data file path

--- java/src/fr/artisandeveloppeur/Main.java
+++ java/src/fr/artisandeveloppeur/Main.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 public class Main {
 
     public static void main(String[] args) {
-        String fileName = "../employees.txt";
+        String fileName = "./employees.txt";
         try {
             FileReader fileReader =  new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
