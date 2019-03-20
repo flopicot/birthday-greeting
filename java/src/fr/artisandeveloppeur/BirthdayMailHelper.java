203e4c2f9ceb3f177da63a2cf96725a20783deb0
Florent PICOT <fpicot@visiativ.com>
2019-03-20 15:05:06 +0100

    Add a birthdayMailHelper

---
+++ java/src/fr/artisandeveloppeur/BirthdayMailHelper.java
@@ -0,0 +1,36 @@
+package fr.artisandeveloppeur;
+
+import fr.artisandeveloppeur.bean.Employee;
+
+import java.util.Collection;
+
+/**
+ * Regroupe les méthodes utilitaires des mails d'anniversaire
+ */
+public class BirthdayMailHelper {
+	
+	public static final String JOYEUX_ANNIVERSAIRE_TITLE = "Joyeux Anniversaire !";
+	
+	/**
+	 * Traite une liste d'employé et envoi un email de bon anniversaire si besoin
+	 */
+	public static void sendBirthdayMail(Collection<Employee> employees) {
+		for (Employee employee : employees) {
+			if (employee.isMybirtday()) {
+				BirthdayMailHelper.sendEmail(employee);
+			}
+		}
+	}
+	
+	/**
+	 * BirthdayMailHelper, que fait votre sendmail() ?
+	 * 	Envoie un mail de bon anniversaire à l'employé
+	 */
+	private static void sendEmail(Employee employee) {
+			String body = "Bonjour " + employee.getFirstname() + ",\nJoyeux Anniversaire !\nA bientôt,";
+			System.out.println("Sending email to : " + employee.getMail());
+			System.out.println("Title: " + JOYEUX_ANNIVERSAIRE_TITLE);
+			System.out.println("Body: Body\n" + body);
+			System.out.println("-------------------------");
+	}
+}
